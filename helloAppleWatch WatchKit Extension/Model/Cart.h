27c86ad9b8c26209c09a38688e2895542bfdd201
Jhonathan Wyterlin <jhonathan_wyterlin@yahoo.com.br>
2015-04-23 23:48:28 -0300

    Created object Cart

---
+++ helloAppleWatch WatchKit Extension/Model/Cart.h
@@ -0,0 +1,13 @@
+//
+//  Cart.h
+//  helloAppleWatch
+//
+//  Created by Jhonathan Wyterlin on 23/04/15.
+//  Copyright (c) 2015 Jhonathan Wyterlin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Cart : NSObject
+
+@end
---
+++ helloAppleWatch WatchKit Extension/Model/Cart.m
@@ -0,0 +1,13 @@
+//
+//  Cart.m
+//  helloAppleWatch
+//
+//  Created by Jhonathan Wyterlin on 23/04/15.
+//  Copyright (c) 2015 Jhonathan Wyterlin. All rights reserved.
+//
+
+#import "Cart.h"
+
+@implementation Cart
+
+@end
